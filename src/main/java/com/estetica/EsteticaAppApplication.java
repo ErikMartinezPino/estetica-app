f8e5a39a2f54d0d6eb798caee7dbac9bc9853115
Erik <171542771+ErikMartinezPino@users.noreply.github.com>
2025-10-06 23:03:30 +0200

    Reinicio del repositorio desde nuevo workspace

---
+++ src/main/java/com/estetica/EsteticaAppApplication.java
@@ -0,0 +1,14 @@
+package com.estetica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsteticaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsteticaAppApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/estetica/EsteticaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estetica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsteticaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
