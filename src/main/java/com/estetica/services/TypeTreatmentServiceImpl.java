93b10bef222247ea99abbcb377db4c62fbc2fd6e
Erik <171542771+ErikMartinezPino@users.noreply.github.com>
2025-10-16 06:50:09 +0200

    Ajustes

--- src/main/java/com/estetica/services/TypeTreatmentServiceImpl.java
+++ src/main/java/com/estetica/services/TypeTreatmentServiceImpl.java
@@ -9,31 +9,35 @@ import com.estetica.model.TypeTreatment;
 import com.estetica.repository.TypeTreatmentRepository;
 
 @Service
-public class TypeTreatmentServiceImpl {
+public class TypeTreatmentServiceImpl implements TypeTreatmentService{
 	
 	//Primero creamos un objeto tipo de tratamiento, que se autoinyecte
 	@Autowired
 	private TypeTreatmentRepository typeTreatmentRepository;
 	
 	//Creamos el listado de tipos de tratamientos
+	@Override
 	public List<TypeTreatment> listTypeTreatment(){
 		List<TypeTreatment> typeTreatment = typeTreatmentRepository.findAll();
 		return typeTreatment;
 	}
 	
 	//Metodo para buscar un tipo de tratamiento
+	@Override
 	public TypeTreatment searchTypeTreatment(Integer idTypeTreatment) {
 		TypeTreatment typeTreatment = typeTreatmentRepository.findById(idTypeTreatment).orElse(null);
 		return typeTreatment;
 	}
 	
 	//Metodo para guardar un tipo de tratamiento
+	@Override
 	public void saveTypeTreatment(TypeTreatment typeTreatment) {
 		typeTreatmentRepository.save(typeTreatment);
 	}
 	
 	//Metodo para eliminar un tipo de tratamiento
-	public void deleteTypeTreatmenr(TypeTreatment typeTreatment) {
+	@Override
+	public void deleteTypeTreatment(TypeTreatment typeTreatment) {
 		typeTreatmentRepository.delete(typeTreatment);
 	}
 
